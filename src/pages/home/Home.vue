a8c17f0486e7afe8dc714745d2fed865cfdd5cd8
xwy <749812994@qq.com>
2019-01-02 15:45:10 +0800

    init

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        home
+        <home-header></home-header>
+    </div>
+</template>
+
+<script>
+import HomeHeader from './components/Header'
+export default {
+    name: 'home',
+    components: {
+        HomeHeader
+    }
+}
+</script>
+
+<style>
+
+</style>
+
